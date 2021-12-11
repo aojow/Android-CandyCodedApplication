0e79e13f72188920c34ea34dce6cb15c33230a1e
ojoaw <lucasyomi@gmail.com>
2021-12-11 08:51:38 -0800

    Completed the project

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -82,6 +82,12 @@ public class MainActivity extends AppCompatActivity {
   // ***
   // TODO - Task 1 - Show Store Information Activity
   // ***
+@Override
+public boolean onOptionsItemsSelected (MenuItem item) {
+    Intent infoIntent= new Intent (package context: this, infoActivity.class);
+    startActivity(InfoIntent);
+    return super.onOptionsItemSelected(item);
+}
 
   private void addCandiesToDatabase(Candy[] candies) {
     SQLiteDatabase db = candyDbHelper.getWritableDatabase();
